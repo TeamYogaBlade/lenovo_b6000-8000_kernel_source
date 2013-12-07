08691f53713ea936a46dbea8b73eefbf181c85e3
Paul Peavyhouse <pv@twistpair.com>
2013-12-07 12:11:51 -0800

    11/14/2013 b6000-8000_source_part2 from: http://mobilesupport.lenovo.com/en-us/products/yoga_tablet_10?tabname=downloads http://download.lenovo.com/consumer/open_source_code/b6000-8000_source_part2.zip

---
+++ b6000-8000_source_part2/kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Sep 16 15:19:38 CST 2013"
+#define LINUX_COMPILE_BY "mtk81316"
+#define LINUX_COMPILE_HOST "mtkscf047"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ b6000-8000_source_part2/kernel/include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.4.5"
+#define BUILD_INFO "ALPS.JB2.MP.V1.17"
+#define BUILD_FINGERPRINT "lenovo89_tb_x8_jb2"
